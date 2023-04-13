a9e88dbe34c45238bf96ff99ac7bb06c35e090b9
João Pinho <jpe.pinho@gmail.com>
2023-04-13 22:39:49 +0000

    epilot gmaps integration with a custom block app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [zipCode, setZipCode] = useState("Im+MediaPark+Cologne")
+
+  useEffect(() => {
+    const zipCodeWatcher = window.setInterval(() => {
+      const zipCodeEl = document.querySelector('div[name="zipCode"] input[type="text"]')
+      
+      if (zipCodeEl && zipCodeEl.value.trim().length > 0) { 
+        setZipCode(zipCodeEl.value) 
+      }
+    }, 100);
+
+    return () => {
+      clearInterval(zipCodeWatcher)
+    }
+  }, [setZipCode])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>Welcome to epilot-maps custom block app!</h1>
+      <h2>Brought to you from GitHub Codespaces & Surge.sh</h2>
+
+      <hr />
+
+      {zipCode}
+
+      <iframe
+        title="google maps"
+        width="100%"
+        height="450"
+        style={{ border:0 }}
+        referrerpolicy="no-referrer-when-downgrade"
+        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyACWsCQTJfbMBsVlBQ64LkP-2yn9Sr4Dpc&q=${zipCode}`}>
+      </iframe>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootNode = document.getElementsByTagName('epilot-gmaps')[0]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (!rootNode) {
+  console.error('Failed to load custom block app, a node with tag name: "epilot-maps" is required.');
+} else {
+  const root = ReactDOM.createRoot(rootNode);
+  
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}
